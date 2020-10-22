91d13a21f3c746205bf0f3a77c9cb12b9670f85f
felipe gomes <felipinhogoma@gmail.com>
2020-10-22 03:32:09 -0300

    Shopping cart service creation

---
+++ shppngcart/src/main/java/com/github/fgsantana/shppngcart/ShppngcartApplication.java
@@ -0,0 +1,13 @@
+package com.github.fgsantana.shppngcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShppngcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShppngcartApplication.class, args);
+	}
+
+}
